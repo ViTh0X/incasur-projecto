f271de2027200228256ed6b6fd338e21d471f3f1
Leo <leonardquispe92@gmail.com>
2025-08-28 22:55:12 -0500

    Agregando usuario asignado al models

--- AdministradorTI/home/models.py
+++ AdministradorTI/home/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class usrforticli(models.Model):
     usuario = models.TextField(max_length=20)
     contrasena = models.TextField(max_length=50)
+    usuarioasignado = models.TextField(max_length=200)
