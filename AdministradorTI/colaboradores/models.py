5d01e627c15d5337889114421ee771d822c908e8
Leo <leonardquispe92@gmail.com>
2025-12-22 12:43:49 -0500

    Cambio si es la ip .36 C:\Patrick 2

--- AdministradorTI/colaboradores/models.py
+++ AdministradorTI/colaboradores/models.py
@@ -18,7 +18,7 @@ class estado_colaboradores(models.Model):
 
 class lista_colaboradores(models.Model):
     codigo_colaborador = models.AutoField(primary_key=True)
-    nombre_colaborador = models.CharField(max_length=150,unique=False)
+    nombre_colaborador = models.CharField(max_length=150,unique=True)
     ip_colaborador = models.ForeignKey(lista_ips,on_delete=models.CASCADE, to_field='ip')
     usuario_sistema = models.CharField(max_length=25,default='SIN ACCESO AL SISTEMA')
     correo = models.CharField(max_length=50,unique=False)
