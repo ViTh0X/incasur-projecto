dd917138423781edf19b27cec0c57dacc6e47e3b
Leo <leonardquispe92@gmail.com>
2025-12-24 11:08:28 -0500

    Arreglando la ejecucion de inventaio software

--- AdministradorTI/utilidades/utilidades_ssh.py
+++ AdministradorTI/utilidades/utilidades_ssh.py
@@ -39,13 +39,7 @@ class SSHManager(logArchivos):
                 self.conexionSSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 self.conexionSSH.connect(hostname=self.hostname,port=self.port,timeout=15,username=self.username,key_filename=self.keyfile,passphrase=self.passphrase)                                                
                 transporte = self.conexionSSH.get_transport()
-                transporte.set_keepalive(20) 
-                comando = 'taskkill /f /im inventario_hardware.exe /t 2>nul & "C:\\Users\\Administrador\\Documents\\TI\\hardware\\inventario_hardware.exe"'
-                stdin, stdout, stderr = self.conexionSSH.exec_command(comando)
-                stdout.read()
-                stderr.read() 
-                exit_status = stdout.channel.recv_exit_status()
-                print("Todos los procesos se eliminaron")                                               
+                transporte.set_keepalive(20)                                                           
                 ruta_archivo_origen_servidor = "/root/inventario_hardware.exe" 
                 ruta_archivo_destino_cliente = "C:/Users/Administrador/Documents/TI/hardware/inventario_hardware.exe"                
                 try:            
@@ -69,13 +63,7 @@ class SSHManager(logArchivos):
                 self.conexionSSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 self.conexionSSH.connect(hostname=self.hostname,port=self.port,timeout=15,username=self.username,key_filename=self.keyfile,passphrase=self.passphrase)                                                
                 transporte = self.conexionSSH.get_transport()
-                transporte.set_keepalive(20)
-                comando = 'taskkill /f /im inventario_software.exe /t 2>nul & "C:\\Users\\Administrador\\Documents\\TI\\software\\inventario_software.exe"'
-                stdin, stdout, stderr = self.conexionSSH.exec_command(comando)
-                stdout.read()
-                stderr.read() 
-                exit_status = stdout.channel.recv_exit_status()
-                print("Todos los procesos se eliminaron")                                
+                transporte.set_keepalive(20)                                          
                 ruta_archivo_origen_servidor = "/root/inventario_software.exe" 
                 ruta_archivo_destino_cliente = "C:/Users/Administrador/Documents/TI/software/inventario_software.exe"                
                 try:            
