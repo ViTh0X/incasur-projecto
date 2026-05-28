dce16ad5e2adcfa030b1f27ee76b769b017081cf
Leonard Quispe <leonardquispe92@gmail.com>
2026-05-28 09:51:12 -0500

    Agregrando el modelo equipos informaticos ti

--- AdministradorTI/inventario_software/views.py
+++ AdministradorTI/inventario_software/views.py
@@ -34,6 +34,7 @@ def listar_inventario_software(request):
         })
         df_pivot=df_software.pivot_table(index=["NombreColaborador","IP"],columns="Categoria",values="Programas",aggfunc=lambda x: "\n".join(f"- {v}"for v in x)).reset_index()
         df_pivot.columns = df_pivot.columns.str.replace(' ','_')
+        df_pivot.columns = df_pivot.columns.str.replace('nan','')
         df_html = df_pivot.replace(r'\n','<br>',regex=True)
         lista_inventarios = df_html.to_dict(orient='records')
         '''inventario_agrupado = {}   
