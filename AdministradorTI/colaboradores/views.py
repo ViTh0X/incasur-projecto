1911b2b1f6be109f78484bb4f3943c0c40c34105
Leonard Quispe <leonardquispe92@gmail.com>
2026-06-16 11:08:36 -0500

    Agregrando el modelo equipos informaticos ti

--- AdministradorTI/colaboradores/views.py
+++ AdministradorTI/colaboradores/views.py
@@ -178,10 +178,10 @@ def cesar_colaborador(request,pk):
             print(e)        
         
         #lista_ids_ips = list(ips.objects.filter(colaborador_asignado=colaborador).values_list('id', flat=True))            
-        # FaltantesRevisionEquiposWindows.objects.filter(codigo_ip__in=lista_ids_ips).delete()        
-        #faltantes_backup_informacion.objects.filter(codigo_ip__in=lista_ids_ips).delete()
-        #faltantes_inventario_hardware.objects.filter(codigo_ip=122).delete()
-        faltantes_inventario_software.objects.filter(codigo_ip__in=[122]).delete()
+        FaltantesRevisionEquiposWindows.objects.filter(codigo_ip__in=pcs_laptops.id).delete()        
+        faltantes_backup_informacion.objects.filter(codigo_ip__in=pcs_laptops.id).delete()
+        faltantes_inventario_hardware.objects.filter(codigo_ip__in=pcs_laptops.id).delete()
+        faltantes_inventario_software.objects.filter(codigo_ip__in=pcs_laptops.id).delete()
         
                    
             
