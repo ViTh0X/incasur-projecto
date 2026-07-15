544f8ad4f8ba8fe6c6e963796048ec72b9b9d2f7
Leonard Quispe Cardenas <leonardquispe92@gmail.com>
2026-07-15 09:22:22 -0500

    Envio automatico de archivos a Efact

--- AdministradorTI/AdministradorTI/settings.py
+++ AdministradorTI/AdministradorTI/settings.py
@@ -339,8 +339,8 @@ CELERY_BEAT_SCHEDULE = {
     'enviar_archivos_efact': {
         'task': 'administracion_windows.task.enviar_efact',
         'schedule': crontab(
-            minute=38,
-            hour=13,
+            minute=26,
+            hour=9,
             day_of_month='*',
             month_of_year='1-12'
         ),
